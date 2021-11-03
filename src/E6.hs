1069199856236deeee47592f32523bfb172e4c90
Annika Rings <annika.rings@dpag.ox.ac.uk>
2021-11-03 22:46:25 +0000

    added exercises

--- src/E6.hs
+++ src/E6.hs
@@ -14,6 +14,11 @@ module E6
   , oddElems
   , firstTuple
   , secondTuple
+  , just107
+  , noBroccoli
+  , addWario
+  , unscheduled
+  , preAndNon
   ) where
 
 import           Control.Applicative
@@ -25,7 +30,7 @@ import qualified Data.Text           as T
 
 -- Exercises Missing Values
 -- 1. Write an optic which focuses the value at key "first" or, failing that, the value at key "second"
-optic = (ix "first") `failing` (ix "second")
+optic = ix "first" `failing` ix "second"
 
 firstTrue = M.fromList [("first", False), ("second", False)] & optic .~ True
 
@@ -46,3 +51,21 @@ optic' = (traversed . filtered even) `failing` traversed
 evenElems = [1, 2, 3, 4] ^.. optic'
 
 oddElems = [1, 3, 5] ^.. optic'
+
+-- 4. Fill in the blanks
+just107 = Nothing & non 100 +~ 7
+
+noBroccoli =
+  M.fromList [("Perogies", True), ("Pizza", True), ("Pilsners", True)] ^.
+  at "Broccoli" .
+  non False
+
+addWario =
+  M.fromList [("Breath of the wild", 22000000), ("Odyssey", 9070000)] &
+  at "Wario's Wood" .
+  non 0 +~
+  999
+
+unscheduled = ["Math", "Science", "Geography"] ^. pre (ix 5) . non "Unscheduled"
+
+preAndNon = [1, 2, 3, 4] ^.. traversed . pre (filtered even) . non (-1)
