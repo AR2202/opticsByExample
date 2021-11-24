9d7caec08eb0a128fd9da92acd5e15204d5419d4
Annika Rings <annika.rings@dpag.ox.ac.uk>
2021-11-24 22:32:32 +0000

    added a Traversal exercise

--- src/E3.hs
+++ src/E3.hs
@@ -16,10 +16,14 @@ module E3
   , amount
   , transactionDelta
   , validateAge
+  , both'
+  , left''
+  , beside'
   ) where
 
 import           Control.Applicative
 import           Control.Lens
+import           Data.Bitraversable
 import           Data.Char
 import qualified Data.Map            as M
 import qualified Data.Set            as S
@@ -71,10 +75,29 @@ newtype BankAccount =
 
 makeLenses ''BankAccount
 
+--1. Rewrite the amount transaction lens manually as teh following traversal:
+--amountT :: Traversal' Transaction Int
 amountT :: Traversal' Transaction Int
 amountT f (Withdrawal n) = Withdrawal <$> f n
 amountT f (Deposit n)    = Deposit <$> f n
 
+--2. Reimplement the both traversal over tuples
+--both :: Traversal (a, a) (b, b) a b
+both' :: Traversal (a, a) (b, b) a b
+both' f (x, y) = bitraverse f f (x, y)
+
+--3. Write the follwing custom traversal
 transactionDelta :: Traversal' Transaction Int
 transactionDelta f (Withdrawal n) = Withdrawal . negate <$> f (negate n)
 transactionDelta f (Deposit n)    = Deposit <$> f n
+
+--4. Implement left :: Traversal (Either a b) (Either a' b) a a'
+left'' :: Traversal (Either a b) (Either a' b) a a'
+left'' f (Left x)  = Left <$> f x
+left'' _ (Right y) = pure (Right y)
+
+--5. BONUS: Reimplement the beside traversal:
+beside' ::
+     Traversal s t a b -> Traversal s' t' a b -> Traversal (s, s') (t, t') a b
+beside' f g handler (a, b) =
+  (,) <$> traverseOf f handler a <*> traverseOf g handler b
